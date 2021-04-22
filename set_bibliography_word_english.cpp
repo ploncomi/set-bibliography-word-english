55baf009a3ca47a5466913adc96b25c375d883fd
Patricio Loncomilla <ploncomi@gmail.com>
2021-04-22 12:31:23 -0400

    Add files via upload
    Initial commit

---
+++ set_bibliography_word_english.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "python_like/split_strip.h"
+#include "python_like/in_operator.h"
+
+int main(int argc, char *argv[])
+{
+  // This code works with .xml files created by JabRef
+  using namespace pl_split_strip;
+  #define in <in_operator::in>
+  #define not_in <in_operator::not_in>
+
+  if (argc != 2)
+  {
+  	std::cout << "This executable needs 1 argument: input xml file" << std::endl;
+  	return 0;
+  }
+  if (".xml" not_in argv[1])
+  {
+  	std::cout << "This executable needs a file with extension xml" << std::endl;
+  	return 0;
+  }
+  std::vector<std::string> nametok = split(argv[1], {"."});
+  nametok[nametok.size()-2] = nametok[nametok.size()-2] + "_english";
+
+  std::string name;
+  for (size_t i=0; i<nametok.size()-1; i++)
+  {
+  	name = name + nametok[i] + ".";
+  }
+  name = name + nametok[nametok.size()-1];
+
+  std::ifstream f_in(argv[1]);
+  std::ofstream f_out(name);
+
+  std::string line;
+  while (std::getline(f_in, line))
+  {
+  	if ("<b:Source>" in line)
+  	  line = line + "<b:LCID>1033</b:LCID>";
+  	f_out << line << std::endl;
+  }
+  #undef in
+  #undef not_in
+  return 0;
+}
+
